bf3917debdea21116dd4c7df4dfe7761adf8d7de
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-21 10:20:19 -0700

    add dish ingredient model spec

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Dish, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
+    it {should have_many :dish_ingredients}
+    it {should have_many(:ingredients).through(:dish_ingredients)}
   end
   describe "relationships" do
     it {should belong_to :chef}
